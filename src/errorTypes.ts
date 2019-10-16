381ab2dc59eb33a24189be6c983a01459eb546e9
ilyamore88 <themorozik@gmail.com>
2019-10-16 20:22:12 +0300

    create error types

---
+++ src/errorTypes.ts
@@ -0,0 +1,61 @@
+/**
+ * Base class for API errors
+ */
+class ApiError extends Error {
+  /**
+   * Http code to send to user
+   */
+  private httpCode: number;
+  protected code: string;
+  /**
+   * Creates error instance
+   * @param {Number} httpCode - http code to send to user
+   */
+  constructor(httpCode: number) {
+    super();
+    this.httpCode = httpCode;
+    this.code = '';
+  }
+}
+
+/**
+ * Throws when trying to log in with the wrong username
+ */
+export class NoUserWithSuchUsernameError extends ApiError {
+  /**
+   * Creates error instance
+   * @param {Number} [httpCode=401] - http code to send to user
+   */
+  constructor(httpCode = 401) {
+    super(httpCode);
+    this.code = 'NO_USER_WITH_SUCH_USERNAME';
+  }
+}
+
+/**
+ * Throws when trying to log in with the wrong password
+ */
+export class WrongUserPasswordError extends ApiError {
+  /**
+   * Creates error instance
+   * @param {Number} [httpCode=401] - http code to send to user
+   */
+  constructor(httpCode = 401) {
+    super(httpCode);
+    this.code = 'WRONG_USER_PASSWORD';
+  }
+}
+
+/**
+ * Throws when trying to register with already registered username
+ */
+export class UsernameDuplicationError extends ApiError {
+  /**
+   * Creates error instance
+   * @param {Number} [httpCode=409] - http code to send to user
+   */
+  constructor(httpCode = 409) {
+    super(httpCode);
+    this.code = 'USERNAME_DUPLICATION';
+  }
+}
