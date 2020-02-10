ab4fb53602c26d1dd6c50a3da8b77b1e1b844733
Nikita Melnikov <nikmel2803@gmail.com>
2020-02-10 12:42:05 +0300

    fix migrations

--- migrations/20200210084942-location-photo-links-as-array.js
+++ migrations/20200210084942-location-photo-links-as-array.js
@@ -3,11 +3,17 @@ module.exports = {
     const locations = await db.collection('locations').find({}).toArray();
 
     await Promise.all(locations.map(async (location) => {
+      let photoArray = [];
+
+      if (location.photoLinks){
+        photoArray = location.photoLinks.split('\n').map(link => link.trim()).filter(link => link)
+      }
+
       await db.collection('locations').updateOne(
         {_id: location._id},
         {
           $set: {
-            photoLinks: location.photoLinks.split('\n').map(link => link.trim()).filter(link => link)
+            photoLinks: photoArray
           }
         }
       )
--- migrations/20200210092601-person-photo-links-as-array.js
+++ migrations/20200210092601-person-photo-links-as-array.js
@@ -3,11 +3,17 @@ module.exports = {
     const persons = await db.collection('persons').find({}).toArray();
 
     await Promise.all(persons.map(async (person) => {
+      let photoArray = [];
+
+      if (person.photoLinks){
+        photoArray = person.photoLinks.split('\n').map(link => link.trim()).filter(link => link)
+      }
+
       await db.collection('persons').updateOne(
         {_id: person._id},
         {
           $set: {
-            photoLinks: person.photoLinks.split('\n').map(link => link.trim()).filter(link => link)
+            photoLinks: photoArray
           }
         }
       )
