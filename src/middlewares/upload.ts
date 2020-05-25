096d68006b851dc7362c6a64f687220239812ca0
Panda0412 <nasb_99@mail.ru>
2020-05-25 14:14:43 +0300

    merge with master & fixes

--- src/middlewares/upload.ts
+++ src/middlewares/upload.ts
@@ -1,37 +1,39 @@
-import multer from 'multer';
+import multer, { FileFilterCallback } from 'multer';
 import multerS3 from 'multer-s3';
 import AWS from 'aws-sdk';
 import path from 'path';
 import { v4 as uuid } from 'uuid';
-import { Router, Request, Response, Express } from 'express';
+import { Router, Request, Response } from 'express';
 
 const router = Router();
 const s3 = new AWS.S3({
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_SECRET_KEY
+  secretAccessKey: process.env.AWS_SECRET_KEY,
 });
 
-console.log(process.env.AWS_ACCESS_KEY_ID, process.env.AWS_SECRET_KEY)
+console.log(process.env.AWS_ACCESS_KEY_ID, process.env.AWS_SECRET_KEY);
 
 /**
  * Allow only JPEG and PNG images
+ *
  * @param {Request} req - Express request for processing
- * @param {Object} file - file info to upload
- * @param {function} cb - callback function
+ * @param {object} file - file info to upload
+ * @param {Function} cb - callback function
  */
-function fileFilter(req: Express.Request, file: any, cb: Function): void {
+function fileFilter(req: Request, file: Express.Multer.File, cb: FileFilterCallback): void {
   if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
     cb(null, true);
   } else {
-    cb(new Error('Invalid file type, only JPEG and PNG is allowed!'), false);
+    cb(new Error('Invalid file type, only JPEG and PNG is allowed!'));
   }
 }
 
 /**
  * Get Multer middleware for image uploading in certain directory
+ *
  * @param {string} folder - folder in S3 bucket to store in
- * @return {Multer}
  */
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 function getUploadMiddleware(folder: string): any {
   return multer({
     fileFilter,
@@ -46,27 +48,28 @@ function getUploadMiddleware(folder: string): any {
         const key = folder + '/' + uuid() + path.extname(file.originalname);
 
         cb(null, key);
-      }
-    })
+      },
+    }),
   });
 }
 
 /**
  * Returns array of uploaded images
+ *
  * @param {Request} req - Express request for processing
  * @param {Response} res - file info to upload
  */
 function filesProcessMiddleware(req: Request, res: Response): void {
   const requestForImage = {
     bucket: 'st-retrospect-images',
-    key: (req.file as Express.MulterS3.File).key
+    key: (req.file as Express.MulterS3.File).key,
   };
 
   res.json({
     success: 1,
     file: {
-      url: process.env.IMAGE_HOSTING_ENDPOINT + Buffer.from(JSON.stringify(requestForImage)).toString('base64')
-    }
+      url: process.env.IMAGE_HOSTING_ENDPOINT + Buffer.from(JSON.stringify(requestForImage)).toString('base64'),
+    },
   });
 }
 
