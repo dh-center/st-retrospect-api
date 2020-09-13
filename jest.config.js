8cfd7896cfbd97d2ae451df8475f47754af02473
Nikita Melnikov <nikmel2803@gmail.com>
2020-09-13 18:47:20 +0300

    add tests for getMultilingualInputTypes.ts

---
+++ jest.config.js
@@ -0,0 +1,39 @@
+/**
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/en/configuration.html
+ */
+module.exports = {
+  /**
+   * The test environment that will be used for testing
+   */
+  testEnvironment: 'node',
+
+  /**
+   * TypeScript support
+   */
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+};
+/**
+ * To prevent problems related to timezones we set node timezone
+ */
+process.env.TZ = 'GMT';
+
+/**
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/en/configuration.html
+ */
+module.exports = {
+  /**
+   * The test environment that will be used for testing
+   */
+  testEnvironment: 'node',
+
+  /**
+   * TypeScript support
+   */
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+};
---
+++ tests/utils/getMultilingualInputTypes.test.ts
@@ -0,0 +1,72 @@
+import { buildSchema } from 'graphql';
+import getMultilingualInputTypes from '../../src/utils/getMultilingualInputTypes';
+
+describe('getMultilingualInputTypes', () => {
+  test('should return correct input types with direct multilingual fields', () => {
+    // language=GraphQL
+    const schema = buildSchema(`
+      directive @multilingual on FIELD_DEFINITION | INPUT_FIELD_DEFINITION
+
+      input CreatePersonInput {
+        firstName: String! @multilingual
+        link: String!
+        lastName: String! @multilingual
+      }
+    `);
+
+    const result = getMultilingualInputTypes(schema);
+
+    expect(result).toBe({
+      CreatePersonInput: ['firstName', 'lastName'],
+    });
+  });
+
+  test('should return correct input types with nested multilingual fields', () => {
+    // language=GraphQL
+    const schema = buildSchema(`
+      directive @multilingual on FIELD_DEFINITION | INPUT_FIELD_DEFINITION
+
+      input CreateLocationInstanceInput {
+        name: String! @multilingual
+      }
+
+      input CreateLocationInput {
+        longitude: Float!
+        lantitude: Float!
+        instance: CreateLocationInstanceInput
+      }
+    `);
+
+    const result = getMultilingualInputTypes(schema);
+
+    expect(result).toBe({
+      CreateLocationInstanceInput: [ 'name' ],
+      CreateLocationInput: [ 'instance' ],
+    });
+  });
+
+  test('should return correct input types with multilingual fields with circular deps', () => {
+    // language=GraphQL
+    const schema = buildSchema(`
+      directive @multilingual on FIELD_DEFINITION | INPUT_FIELD_DEFINITION
+
+      input AInput {
+        name: String! @multilingual
+        b: BInput!
+      }
+
+      input BInput {
+        longitude: Float!
+        lantitude: Float!
+        instance: AInput!
+      }
+    `);
+
+    const result = getMultilingualInputTypes(schema);
+
+    expect(result).toBe({
+      AInput: ['name', 'b'],
+      BInput: ['instance', 'instance'],
+    });
+  });
+});
