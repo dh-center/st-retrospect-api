89b7260f674d6f84ac9faea0d6946b948de36e73
ilyamore88 <themorozik@gmail.com>
2020-09-26 15:26:43 +0300

    add resolver for creating mutation

--- src/resolvers/relations.ts
+++ src/resolvers/relations.ts
@@ -1,5 +1,6 @@
 import { ObjectId } from 'mongodb';
-import { MultilingualString } from '../types/graphql';
+import { CreateMutationPayload, MultilingualString, ResolverContextBase } from '../types/graphql';
+import { PersonDBScheme } from './persons';
 
 /**
  * Relation's database scheme
@@ -61,23 +62,53 @@ export interface RelationSynonymDBScheme {
   name: MultilingualString;
 }
 
-export default {
-  RelationType: {
-    /**
-     * Resolver for relation type synonyms
-     *
-     * @param relation - the object that contains the result returned from the resolver on the parent field
-     */
-    synonyms(
-      relation: RelationTypeDBScheme
-    ): (MultilingualString | null)[] {
-      return relation.synonyms.map((synonym) => {
-        if (!synonym) {
-          return null;
-        }
+const RelationMutations = {
+  /**
+   * Create new relation
+   *
+   * @param parent - the object that contains the result returned from the resolver on the parent field
+   * @param input - relation object
+   * @param db - MongoDB connection to make queries
+   */
+  async create(
+    parent: undefined,
+    { input }: { input: RelationDBScheme },
+    { db }: ResolverContextBase
+  ): Promise<CreateMutationPayload<RelationDBScheme>> {
+    const relation = (await db.collection<RelationDBScheme>('relations').insertOne(input)).ops[0];
 
-        return synonym.name;
-      });
-    },
+    return {
+      recordId: relation._id,
+      record: relation,
+    };
   },
+};
+
+const RelationType = {
+  /**
+   * Resolver for relation type synonyms
+   *
+   * @param relation - the object that contains the result returned from the resolver on the parent field
+   */
+  synonyms(
+    relation: RelationTypeDBScheme
+  ): (MultilingualString | null)[] {
+    return relation.synonyms.map((synonym) => {
+      if (!synonym) {
+        return null;
+      }
+
+      return synonym.name;
+    });
+  },
+};
+
+const Mutation = {
+  relation: (): Record<string, undefined> => ({}),
+};
+
+export default {
+  RelationType,
+  Mutation,
+  RelationMutations,
 };
