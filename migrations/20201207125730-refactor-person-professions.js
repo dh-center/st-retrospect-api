239ec67af5acd65b3e46799b08855a03bb13db65
Nikita Melnikov <nikmel2803@gmail.com>
2020-12-07 17:23:38 +0300

    add migrations

---
+++ migrations/20201207125730-refactor-person-professions.js
@@ -0,0 +1,51 @@
+const asyncForEach = require('./utils/asyncForEach');
+
+module.exports = {
+  async up(db, client) {
+    console.log('20201207125730-refactor-person-professions:up');
+    const persons = await db.collection('persons').find({}).toArray();
+
+    await asyncForEach(persons, async (person) => {
+      console.log(`\tprocess person with id ${person._id}`);
+      const professions = person.professions || [];
+      const professionsRu = professions.map(profession => profession.ru)
+      const professionsEn = professions.map(profession => profession.en)
+      const newProfessions = {
+        ru: professionsRu,
+        en: professionsEn
+      };
+
+      await db.collection('persons').updateOne({ _id: person._id }, {
+        $set: {
+          professions: newProfessions,
+        },
+      });
+    });
+  },
+
+  async down(db, client) {
+    console.log('20201207125730-refactor-person-professions:down');
+    const persons = await db.collection('persons').find({}).toArray();
+
+    await asyncForEach(persons, async (person) => {
+      console.log(`\tprocess person with id ${person._id}`);
+      const professions = person.professions || {ru: [], en: []}
+      const length = Math.max(professions.en.length, professions.ru.length)
+
+      const newProfessions = []
+
+      for (let i = 0; i < length; i++) {
+        newProfessions.push({
+          ru: professions.ru && professions.ru[i],
+          en: professions.en && professions.en[i]
+        })
+      }
+
+      await db.collection('persons').updateOne({ _id: person._id }, {
+        $set: {
+          professions: newProfessions,
+        },
+      });
+    });
+  }
+};
