7e6ed399156df0b5e2a506ebbd3b4f4f9e42f01f
Nikita Melnikov <nikmel2803@gmail.com>
2020-12-18 20:51:31 +0300

    add data validation for facebook auth

--- src/routes/oauth/facebook.ts
+++ src/routes/oauth/facebook.ts
@@ -2,60 +2,89 @@ import { Router } from 'express';
 import axios from 'axios';
 import { generateUserToken } from '../../utils/jwt';
 import { getCollection } from '../../db';
+import * as z from 'zod';
+import { WrongAuthData } from '../../errorTypes';
 
 const router = Router();
 
-router.post('/oauth/facebook/callback', async (req, res) => {
-  console.log(req.query);
+/**
+ * Schema for the query params for Facebook auth handler
+ */
+const FacebookAuthDataScheme = z.object({
+  token: z.string(),
+});
+
+/**
+ * Schema for the Facebook user data
+ */
+const FacebookUserData = z.object({ /* eslint-disable @typescript-eslint/naming-convention */
+  id: z.string(),
+  name: z.string(),
+  first_name: z.string(),
+  last_name: z.string(),
+  email: z.string(),
+  picture: z.object({
+    data: z.object({
+      url: z.string(),
+    }),
+  }),
+});
+
+router.post('/oauth/facebook/callback', async (req, res, next) => {
+  let authData;
+
   try {
-    const response = await axios.get(`https://graph.facebook.com/v9.0/me?access_token=${req.query.token}&fields=id,name,first_name,last_name,email,picture.type(large)`);
-
-    const userData = response.data;
-
-    console.log(userData);
-
-    const collection = await getCollection('users');
-
-    const existedUser = await collection.findOne({
-      $or: [
-        {
-          email: req.query.email as string,
-        },
-        {
-          'auth.facebook.id': +userData.id,
-        },
-      ],
-    });
-
-    if (!existedUser) {
-      /**
-       * Create new user in database
-       */
-      const newUser = (await collection.insertOne({
-        firstName: req.query.first_name as string,
-        lastName: req.query.last_name as string,
-        email: req.query.email as string,
-        username: req.query.email as string, // @todo think about username uniques
-        auth: {
-          facebook: {
-            id: +userData.id,
-          },
-        },
-      })).ops[0];
-
-      const accessToken = generateUserToken(newUser);
-
-      return res.json({ data: { accessToken } });
-    }
-
-    const accessToken = generateUserToken(existedUser);
-
-    return res.json({ data: { accessToken } });
+    authData = FacebookAuthDataScheme.parse(req.query);
   } catch (e) {
-    console.log(e);
+    return next(new WrongAuthData());
   }
 
-  res.sendStatus(400);
+  const response = await axios.get(`https://graph.facebook.com/v9.0/me?access_token=${authData.token}&fields=id,name,first_name,last_name,email,picture.type(large)`);
+  let userData;
+
+  try {
+    userData = FacebookUserData.parse(response.data);
+  } catch {
+    return next(new WrongAuthData());
+  }
+
+  const collection = await getCollection('users');
+
+  const existedUser = await collection.findOne({
+    $or: [
+      {
+        email: userData.email,
+      },
+      {
+        'auth.facebook.id': +userData.id,
+      },
+    ],
+  });
+
+  let accessToken;
+
+  if (!existedUser) {
+    /**
+     * Create new user in database
+     */
+    const newUser = (await collection.insertOne({
+      firstName: userData.first_name,
+      lastName: userData.last_name,
+      email: userData.email,
+      username: userData.email,
+      auth: {
+        facebook: {
+          id: +userData.id,
+        },
+      },
+    })).ops[0];
+
+    accessToken = generateUserToken(newUser);
+  } else {
+    accessToken = generateUserToken(existedUser);
+  }
+
+  return res.json({ data: { accessToken } });
 });
 
 export default router;
