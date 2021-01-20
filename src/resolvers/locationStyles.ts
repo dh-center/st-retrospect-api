810681b9c33af1a201524cdd22efbad9f3c59c32
ilyamore88 <themorozik@gmail.com>
2021-01-20 16:13:39 +0300

    add resolver for update mutation

--- src/resolvers/locationStyles.ts
+++ src/resolvers/locationStyles.ts
@@ -1,7 +1,13 @@
-import { CreateLocationStyleInput } from '../generated/graphql';
-import { CreateMutationPayload, ResolverContextBase } from '../types/graphql';
+import {
+  CreateLocationStyleInput,
+  UpdateLocationStyleInput
+} from '../generated/graphql';
+import { CreateMutationPayload, ResolverContextBase, UpdateMutationPayload } from '../types/graphql';
 import { LocationStyleDBScheme } from './locations';
 import emptyMutation from '../utils/emptyMutation';
+import { ObjectId } from 'mongodb';
+import { UserInputError } from 'apollo-server-express';
+import mergeWithCustomizer from '../utils/mergeWithCustomizer';
 
 const LocationStyleMutations = {
   /**
@@ -21,11 +27,52 @@ const LocationStyleMutations = {
      */
     const locationStyle = (await collection('locationstyles').insertOne(input)).ops[0];
 
-    // await sendNotify('LocationStyle', 'location', db, user, 'create', locationStyle);
-
     return {
       recordId: locationStyle._id,
       record: locationStyle,
+    };
+  },
+
+  /**
+   * Updates location style
+   *
+   * @param parent - this is the return value of the resolver for this field's parent
+   * @param args - contains all GraphQL arguments provided for this field
+   * @param context - this object is shared across all resolvers that execute for a particular operation
+   */
+  async update(
+    parent: undefined,
+    { input }: { input: UpdateLocationStyleInput },
+    { collection }: ResolverContextBase
+  ): Promise<UpdateMutationPayload<LocationStyleDBScheme>> {
+    const { id, ...rest } = input;
+    const newInput: LocationStyleDBScheme = {
+      _id: new ObjectId(id),
+      ...rest,
+    };
+
+    const originalLocationStyle = await collection('locationstyles').findOne({
+      _id: newInput._id,
+    });
+
+    if (!originalLocationStyle) {
+      throw new UserInputError('There is no location style with such id: ' + newInput._id);
+    }
+
+    const locationStyle = await collection('locationstyles').findOneAndUpdate(
+      { _id: newInput._id },
+      {
+        $set: mergeWithCustomizer(originalLocationStyle, newInput),
+      },
+      { returnOriginal: false });
+
+    if (!locationStyle.value) {
+      throw new UserInputError('Can\'t update location style with such id: ' + newInput._id);
+    }
+
+    return {
+      recordId: newInput._id,
+      record: locationStyle.value,
     };
   },
 };
