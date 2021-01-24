0f32cafad7dd89b4e0c71508aea8334ffe7ddf03
Nikita Melnikov <nikmel2803@gmail.com>
2021-01-24 23:26:36 +0300

    remove Route entity from API

--- src/resolvers/users.ts
+++ src/resolvers/users.ts
@@ -1,6 +1,5 @@
 import { ObjectId } from 'mongodb';
 import { ResolverContextBase } from '../types/graphql';
-import { RouteDBScheme } from './routes';
 import { InvalidAccessToken } from '../errorTypes';
 
 /**
@@ -46,16 +45,6 @@ export interface UserDBScheme {
    * User last name
    */
   lastName?: string | null;
-
-  /**
-   * Saved routes ids array
-   */
-  savedRouteIds?: ObjectId[] | null;
-
-  /**
-   * Liked routes ids array
-   */
-  likedRouteIds?: ObjectId[] | null;
 
   /**
    * Information about auth providers
@@ -112,113 +101,6 @@ const Query = {
   },
 };
 
-const Mutation = {
-  /**
-   * Add route to saved
-   *
-   * @param parent - this is the return value of the resolver for this field's parent
-   * @param args - contains all GraphQL arguments provided for this field
-   * @param context - this object is shared across all resolvers that execute for a particular operation
-   */
-  async saveRoute(parent: undefined, { routeId }: { routeId: string }, { db, user }: ResolverContextBase): Promise<UserDBScheme> {
-    return (await db.collection('users').findOneAndUpdate({ _id: new ObjectId(user.id) }, {
-      $addToSet: { savedRouteIds: new ObjectId(routeId) },
-    },
-    {
-      returnOriginal: false,
-    })).value;
-  },
-
-  /**
-   * Delete route from saved
-   *
-   * @param parent - this is the return value of the resolver for this field's parent
-   * @param args - contains all GraphQL arguments provided for this field
-   * @param context - this object is shared across all resolvers that execute for a particular operation
-   */
-  async deleteRouteFromSaved(parent: undefined, { routeId }: { routeId: string }, { db, user }: ResolverContextBase): Promise<UserDBScheme> {
-    return (await db.collection('users').findOneAndUpdate({ _id: new ObjectId(user.id) },
-      {
-        $pull: { savedRouteIds: new ObjectId(routeId) },
-      },
-      {
-        returnOriginal: false,
-      })).value;
-  },
-
-  /**
-   * Add route to liked
-   *
-   * @param parent - this is the return value of the resolver for this field's parent
-   * @param args - contains all GraphQL arguments provided for this field
-   * @param context - this object is shared across all resolvers that execute for a particular operation
-   */
-  async likeRoute(parent: undefined, { routeId }: { routeId: string }, { db, user }: ResolverContextBase): Promise<UserDBScheme> {
-    return (await db.collection('users').findOneAndUpdate({ _id: new ObjectId(user.id) },
-      {
-        $addToSet: { likedRouteIds: new ObjectId(routeId) },
-      },
-      {
-        returnOriginal: false,
-      })).value;
-  },
-
-  /**
-   * Dislike route
-   *
-   * @param parent - this is the return value of the resolver for this field's parent
-   * @param args - contains all GraphQL arguments provided for this field
-   * @param context - this object is shared across all resolvers that execute for a particular operation
-   */
-  async dislikeRoute(parent: undefined, { routeId }: { routeId: string }, { db, user }: ResolverContextBase): Promise<UserDBScheme> {
-    return (await db.collection('users').findOneAndUpdate({ _id: new ObjectId(user.id) },
-      {
-        $pull: { likedRouteIds: new ObjectId(routeId) },
-      },
-      {
-        returnOriginal: false,
-      })).value;
-  },
-};
-
-const User = {
-  /**
-   * Returns saved routes
-   *
-   * @param user - resolved user from parent resolver
-   * @param args - contains all GraphQL arguments provided for this field
-   * @param context - this object is shared across all resolvers that execute for a particular operation
-   */
-  async savedRoutes(user: UserDBScheme, args: undefined, { dataLoaders }: ResolverContextBase): Promise<RouteDBScheme[]> {
-    if (!user.savedRouteIds) {
-      return [];
-    }
-
-    const savedRoutes = await dataLoaders.routesById.loadMany(user.savedRouteIds.map(id => id.toString()));
-
-    return savedRoutes.filter(Boolean) as RouteDBScheme[];
-  },
-
-  /**
-   * Returns liked routes
-   *
-   * @param user - resolved user from parent resolver
-   * @param args - contains all GraphQL arguments provided for this field
-   * @param context - this object is shared across all resolvers that execute for a particular operation
-   */
-  async likedRoutes(user: UserDBScheme, args: undefined, { dataLoaders }: ResolverContextBase): Promise<RouteDBScheme[]> {
-    if (!user.likedRouteIds) {
-      return [];
-    }
-
-    const likedRoutes = await dataLoaders.routesById.loadMany(user.likedRouteIds.map(id => id.toString()));
-
-    return likedRoutes.filter(Boolean) as RouteDBScheme[];
-  },
-};
-
 export default {
   Query,
-  User,
-  Mutation,
 };
