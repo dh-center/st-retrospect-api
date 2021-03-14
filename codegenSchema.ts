120d32429803604845348280116f91c40c9e3822
Nikita Melnikov <nikmel2803@gmail.com>
2021-03-14 19:05:33 +0300

    setup codegen

---
+++ codegenSchema.ts
@@ -0,0 +1,31 @@
+import multilingualDirective from './src/directives/multilingual';
+import paginationDirective from './src/directives/pagination';
+import fromFieldDirective from './src/directives/fromField';
+import authCheckDirective from './src/directives/authСheck';
+import adminCheckDirective from './src/directives/adminCheck';
+import dataLoaderDirective from './src/directives/dataloaders';
+import createDirectiveDefault from '@codexteam/graphql-directive-default';
+
+import globalIdResolver from './src/globalIdResolver';
+import toGlobalIdDirective from './src/directives/toGlobalId';
+
+import { makeExecutableSchema } from '@graphql-tools/schema';
+import typeDefs from './src/typeDefs';
+
+/**
+ * Export scheme for GraphQL Code Generator
+ */
+export default makeExecutableSchema({
+  typeDefs,
+  schemaTransforms: [
+    globalIdResolver,
+    toGlobalIdDirective('toGlobalId'),
+    paginationDirective('pagination'),
+    multilingualDirective('multilingual'),
+    fromFieldDirective('fromField'),
+    authCheckDirective('authCheck'),
+    adminCheckDirective('adminCheck'),
+    dataLoaderDirective('dataLoader'),
+    createDirectiveDefault().schemaTransformer,
+  ],
+});
