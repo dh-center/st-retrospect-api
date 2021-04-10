65fc488ec91795104d2a5a34c9379023648c7ad8
Nikita Melnikov <nikmel2803@gmail.com>
2021-04-10 23:44:32 +0300

    add api key support

--- src/utils/getElasticClient.ts
+++ src/utils/getElasticClient.ts
@@ -1,4 +1,4 @@
-import { Client } from '@elastic/elasticsearch';
+import { Client, ClientOptions } from '@elastic/elasticsearch';
 
 /**
  * Creates configured ElasticSearch client
@@ -10,5 +10,17 @@ export default function getElasticClient(): Client {
     throw new Error('No elastic search endpoint configured. Please check environment variables');
   }
 
-  return new Client({ node: process.env.ELASTICSEARCH_ENDPOINT });
+  const options: ClientOptions = {
+    node: process.env.ELASTICSEARCH_ENDPOINT,
+  };
+
+  const apiKey = process.env.ELASTICSEARCH_API_KEY;
+
+  if (apiKey) {
+    options.auth = {
+      apiKey,
+    };
+  }
+
+  return new Client(options);
 }
