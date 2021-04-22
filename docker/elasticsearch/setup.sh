f66599919fa227aa69b2564e35ac12b96ccee9d7
Nikita Melnikov <nikmel2803@gmail.com>
2021-04-22 15:35:07 +0300

    rename view and index

--- docker/elasticsearch/setup.sh
+++ docker/elasticsearch/setup.sh
@@ -6,4 +6,4 @@ index_prefix=${2:-'retrospect'}
 
 curl -XDELETE "${endpoint}/${index_prefix}.*"
 
-curl -XPUT -H 'Content-Type: application/json' "${endpoint}/${index_prefix}.locations_view" -d @./indexes/locations_view.json
+curl -XPUT -H 'Content-Type: application/json' "${endpoint}/${index_prefix}.locations" -d @./indexes/locations.json
