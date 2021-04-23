f01ec46082eca8b075b00f96f7c14878f94935ab
Nikita Melnikov <nikmel2803@gmail.com>
2021-04-23 19:24:22 +0300

    setup new index

--- docker/elasticsearch/setup.sh
+++ docker/elasticsearch/setup.sh
@@ -7,3 +7,5 @@ index_prefix=${2:-'retrospect'}
 curl -XDELETE "${endpoint}/${index_prefix}.*"
 
 curl -XPUT -H 'Content-Type: application/json' "${endpoint}/${index_prefix}.locations" -d @./indexes/locations.json
+
+curl -XPUT -H 'Content-Type: application/json' "${endpoint}/${index_prefix}.location_instances" -d @./indexes/location_instances.json
