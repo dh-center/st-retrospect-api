a567af5b56673e45bc69c9724d2a6cc8ecc8788c
Nikita Melnikov <nikmel2803@gmail.com>
2021-04-23 19:25:13 +0300

    add new index name

--- src/utils/elasticIndexes.ts
+++ src/utils/elasticIndexes.ts
@@ -2,6 +2,7 @@ const esIndexPrefix = process.env.ELASTICSEARCH_INDEX_PREFIX || 'retrospect';
 
 const elasticIndexes = {
   locations: `${esIndexPrefix}.locations`,
+  locationInstances: `${esIndexPrefix}.location_instances`,
 };
 
 export default elasticIndexes;
