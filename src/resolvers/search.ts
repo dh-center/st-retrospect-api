fdfd4f3d2513036a2a3790d525bee90f08aa4869
Nikita Melnikov <nikmel2803@gmail.com>
2021-04-23 19:28:00 +0300

    add search resolvers

--- src/resolvers/search.ts
+++ src/resolvers/search.ts
@@ -5,42 +5,6 @@ import elasticIndexes from '../utils/elasticIndexes';
 import { ResolverContextBase } from '../types/graphql';
 
 const Query = {
-  // /**
-  //  * Search for entities by search query
-  //  *
-  //  * @param parent - this is the return value of the resolver for this field's parent
-  //  * @param args - contains all GraphQL arguments provided for this field
-  //  */
-  // async search(parent: undefined, args: QuerySearchArgs) { // todo add return value
-  //   const client = getElasticClient();
-  //
-  //   const result = await client.search({
-  //     index: elasticIndexes.locationsView,
-  //     body: {
-  //       query: {
-  //         // eslint-disable-next-line @typescript-eslint/naming-convention
-  //         multi_match: {
-  //           query: args.input.query,
-  //           fields: [
-  //             'name.*^2',
-  //             'description.*',
-  //             '*',
-  //           ],
-  //         },
-  //       },
-  //     },
-  //   });
-  //
-  //   // todo fix types
-  //   return result.body.hits.hits.map((hit: any) =>
-  //     ({
-  //       ...hit._source,
-  //       __typename: 'LocationInstance',
-  //     })
-  //   );
-  // },
-
-
   /**
    * Search for entities by search query
    *
@@ -70,37 +34,13 @@ const Query = {
     }
 
     const result = await client.search({
-      index: elasticIndexes.locations,
+      index: elasticIndexes.locationInstances,
       body: {
         from: input.windowedPagination?.skip,
         size: input.windowedPagination?.first,
         query,
-        suggest: {
-          text: input.query,
-          phrase_suggester: {
-            phrase: {
-              field: 'name.ru.trigram',
-              size: 1,
-              gram_size: 3,
-              direct_generator: [
-                {
-                  field: 'name.ru.trigram',
-                  suggest_mode: 'always',
-                },
-              ],
-              highlight: {
-                pre_tag: '<b>',
-                post_tag: '</b>',
-              },
-            },
-          },
-        },
       },
     });
-
-    // console.log(JSON.stringify(result.body, null, 2));
-    // console.log(result.body.hits.hits.length);
-    // console.log(result.body.hits.total.value);
 
     // todo fix types
     return {
@@ -118,18 +58,32 @@ const Query = {
   },
 
   /**
-   *
-   *
    * @param parent - this is the return value of the resolver for this field's parent
    * @param args - contains all GraphQL arguments provided for this field
+   * @param context - this object is shared across all resolvers that execute for a particular operation
    */
-  async locationsSearch(parent: undefined, { input }: QueryLocationsInstancesSearchArgs) { // todo add return value
+  async locationsSearch(parent: undefined, { input }: QueryLocationsInstancesSearchArgs, { collection }: ResolverContextBase) { // todo add return value
     const client = getElasticClient();
     let query;
 
     if (!input.query) {
-      query = {
-        match_all: {},
+      const locations = await collection('locations')
+        .find()
+        .skip(input.windowedPagination?.skip || 0)
+        .limit(input.windowedPagination?.first || 10)
+        .toArray();
+
+
+      return {
+        edges:
+          locations.map((location) => {
+            return ({
+              node: location,
+            });
+          }),
+        totalCount: await collection('locations')
+          .find()
+          .count(),
       };
     } else {
       query = {
@@ -150,33 +104,9 @@ const Query = {
       body: {
         from: input.windowedPagination?.skip,
         size: input.windowedPagination?.first,
-        query,
-        // suggest: {
-        //   text: input.query,
-        //   phrase_suggester: {
-        //     phrase: {
-        //       field: 'instances.name.ru.trigram',
-        //       size: 1,
-        //       gram_size: 3,
-        //       direct_generator: [
-        //         {
-        //           field: 'name.ru.trigram',
-        //           suggest_mode: 'always',
-        //         },
-        //       ],
-        //       highlight: {
-        //         pre_tag: '<b>',
-        //         post_tag: '</b>',
-        //       },
-        //     },
-        //   },
-        // },
+        query
       },
     });
-
-    // console.log(JSON.stringify(result.body, null, 2));
-    // console.log(result.body.hits.hits.length);
-    // console.log(result.body.hits.total.value);
 
     // todo fix types
 
