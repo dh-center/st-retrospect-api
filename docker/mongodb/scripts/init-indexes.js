0e2c47bc13255c3f1d68dea0246bc197808c4f18
Nikita Melnikov <nikmel2803@gmail.com>
2021-06-06 18:10:57 +0300

    add year field to relations

--- docker/mongodb/scripts/init-indexes.js
+++ docker/mongodb/scripts/init-indexes.js
@@ -14,6 +14,26 @@ db.getCollectionNames().forEach(function (collName) {
 
 db.createView('location_instances_denormalized', 'location_instances', [
   {
+    '$addFields': {
+      'yearMatch': {
+        '$regexFind': {
+          'input': '$startDate',
+          'regex': new RegExp('d{4}')
+        }
+      }
+    }
+  }, {
+    '$addFields': {
+      'year': {
+        '$toInt': '$yearMatch.match'
+      }
+    }
+  }, {
+    '$project': {
+      'yearMatch': 0
+    }
+  },
+  {
     '$lookup': {
       'from': 'locationtypes',
       'localField': 'locationTypesId',
@@ -52,47 +72,62 @@ db.createView('location_instances_denormalized', 'location_instances', [
 
 db.createView('locations_denormalized', 'location_instances', [
   {
+    '$lookup': {
+      'from': 'persons',
+      'localField': 'personId',
+      'foreignField': '_id',
+      'as': 'person'
+    }
+  }, {
+    '$unwind': {
+      'path': '$person',
+      'preserveNullAndEmptyArrays': true
+    }
+  }, {
+    '$lookup': {
+      'from': 'location_instances',
+      'localField': 'locationInstanceId',
+      'foreignField': '_id',
+      'as': 'locationInstance'
+    }
+  }, {
+    '$unwind': {
+      'path': '$locationInstance',
+      'preserveNullAndEmptyArrays': true
+    }
+  }, {
+    '$lookup': {
+      'from': 'relationtypes',
+      'localField': 'relationId',
+      'foreignField': '_id',
+      'as': 'relationType'
+    }
+  }, {
+    '$unwind': {
+      'path': '$relationType',
+      'preserveNullAndEmptyArrays': true
+    }
+  }, {
+    '$addFields': {
+      'yearMatch': {
+        '$regexFind': {
+          'input': '$startDate',
+          'regex': new RegExp('d{4}')
+        }
+      }
+    }
+  }, {
+    '$addFields': {
+      'year': {
+        '$toInt': '$yearMatch.match'
+      }
+    }
+  }, {
     '$project': {
-      'source': 0
+      'yearMatch': 0
     }
-  },
-  {
-    '$lookup': {
-      'from': 'locationtypes',
-      'localField': 'locationTypesId',
-      'foreignField': '_id',
-      'as': 'locationTypes'
-    }
-  },
-  {
-    '$lookup': {
-      'from': 'locationstyles',
-      'localField': 'locationStyleId',
-      'foreignField': '_id',
-      'as': 'locationStyle'
-    }
-  },
-  {'$unwind': {'path': '$locationStyle', 'preserveNullAndEmptyArrays': true}}, {
-    '$lookup': {
-      'from': 'tags',
-      'localField': 'tagIds',
-      'foreignField': '_id',
-      'as': 'tags'
-    }
-  },
-  {'$group': {'_id': '$locationId', 'instances': {'$push': '$$ROOT'}}},
-  {
-    '$lookup': {
-      'from': 'locations',
-      'localField': '_id',
-      'foreignField': '_id',
-      'as': 'parent'
-    }
-  },
-  {'$unwind': {'path': '$parent'}},
-  {'$replaceRoot': {'newRoot': {'$mergeObjects': ['$$ROOT', '$parent']}}},
-  {'$project': {'parent': 0}}
-])
+  }
+]w)
 
 db.createView('relations_denormalized', 'relations', [
   {
