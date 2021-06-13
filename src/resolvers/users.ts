b7bbd21d3c152b084ea04240286b39d1054c85d5
ilyamore88 <themorozik@gmail.com>
2021-06-13 17:05:46 +0300

    fix bug

--- src/resolvers/users.ts
+++ src/resolvers/users.ts
@@ -386,9 +386,7 @@ const UserMutations = {
     const newFriend = await collection('users').findOne(
       {
         _id: secondUserId,
-        friendPendingRequestsIds: {
-          $elemMatch: currentUserId,
-        },
+        friendPendingRequestsIds: currentUserId,
       }
     );
 
